cce6be61ba827b9a836861acd7c9f6059908eee4
Luca Giusto <168096340+Just-Luca@users.noreply.github.com>
2026-02-03 10:41:39 +0100

    Add environment variable loading and constants

--- src/constants.py
+++ src/constants.py
@@ -1,1 +1,10 @@
+import os
+from datetime import date
+from dotenv import load_dotenv
 
+load_dotenv()
+
+API_KEY = os.getenv("apod_key")
+safe_date = date(2000, 1, 1)
+SAVE_DIR = "your_dir_path"
+url = "https://api.nasa.gov/planetary/apod"
